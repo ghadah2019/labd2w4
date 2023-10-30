8a67a0741f4f95a2756a7eeb9d38dcb2af58fbaf
ghadah2019 <ghada__o@hotmail.com>
2023-10-30 13:10:27 +0300

    vvv

---
+++ main.js
@@ -0,0 +1,47 @@
+for(i  = 0; i<= 25; i++ )
+{
+if (i %  2 ==  0)
+{
+    console.log(i);
+}
+}
+
+for(i  = 1; i<= 10; i++ )
+{
+squarei  = i * i
+console.log(squarei);
+}
+
+for(i  = 1; i<= 20; i++ ) {
+    if (i %  2 ==  1)
+{
+    console.log(i);
+}
+
+}
+
+
+
+var num1 = 0;
+var num2 = 1;
+var num3
+
+for (let i = 0; i < 10; i++) {
+    console.log(num1)
+    num3 = num2 + num1
+    num1 = num2
+    num2 =  num3
+  
+
+}
+
+for (i = 0; i <= 3;  i++)
+{
+    for(j = 1; j <= 10; j++){
+        console.log(`${i} * ${j} = ` + i * j);
+    }
+}
+
+
+
+
